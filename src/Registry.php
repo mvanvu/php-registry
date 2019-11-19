2ffaa5a7d7d69c495b9195cfac27aab4bbb98d14
Vu <vumv@solidres.com>
2019-11-19 17:25:06 +0700

    Intial commit

---
+++ src/Registry.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace MaiVu\Php;
+
+class Registry
+{
+	/** @var array */
+	protected $data = [];
+
+	public function __construct($data = null)
+	{
+		$this->data = $this->parse($data);
+	}
+
+	/**
+	 * @param $data
+	 *
+	 * @return array
+	 */
+	public function parse($data)
+	{
+		return self::parseData($data);
+	}
+
+	public static function parseData($data)
+	{
+		if ($data instanceof Registry)
+		{
+			$data = $data->toArray();
+		}
+		elseif (is_object($data))
+		{
+			$data = (array) $data;
+		}
+		elseif (is_string($data))
+		{
+			if (strpos($data, '{') === 0 || strpos($data, '[') === 0)
+			{
+				$data = json_decode($data, true) ?: [];
+			}
+			elseif (is_file($data))
+			{
+				if (preg_match('/\.php$/', $data))
+				{
+					$data = include $data;
+				}
+				elseif (preg_match('/\.json$/', $data))
+				{
+					$data = json_decode(file_get_contents($data), true) ?: [];
+				}
+			}
+		}
+
+		if (empty($data))
+		{
+			return [];
+		}
+
+		return is_array($data) ? $data : [$data];
+	}
+
+	public function merge($data)
+	{
+		$this->data = array_merge($this->data, $this->parse($data));
+
+		return $this;
+	}
+
+	public function get($path, $defaultValue = null)
+	{
+		if (false === strpos($path, '.'))
+		{
+			return array_key_exists($path, $this->data) ? $this->data[$path] : $defaultValue;
+		}
+
+		$keys = explode('.', $path);
+		$data = $this->data;
+
+		foreach ($keys as $key)
+		{
+			if (!isset($data[$key]))
+			{
+				return $defaultValue;
+			}
+
+			$data = $data[$key];
+		}		
+
+		return $data;
+	}
+
+	public function set($path, $value)
+	{
+		if (false === strpos($path, '.'))
+		{
+			$this->data[$path] = $value;
+		}
+		else
+		{
+			$keys = explode('.', $path);
+			$data = &$this->data;
+
+			foreach ($keys as $key)
+			{
+				if (!isset($data[$key]))
+				{
+					$data[$key] = [];
+				}
+
+				$data = &$data[$key];
+			}
+
+			$data = $value;
+		}
+
+		return $this;
+	}
+
+	public function has($path)
+	{
+		if (false === strpos($path, '.'))
+		{
+			return array_key_exists($path, $this->data);
+		}
+
+		$keys = explode('.', $path);
+		$data = $this->data;
+
+		foreach ($keys as $key)
+		{
+			if (!array_key_exists($key, $data))
+			{
+				return false;
+			}
+
+			$data = $data[$key];
+		}
+
+		return true;
+	}
+
+	public function toArray()
+	{
+		return $this->data;
+	}
+
+	public function toString()
+	{
+		return json_encode($this->data);
+	}
+
+	public function __toString()
+	{
+		return $this->toString();
+	}
+}
